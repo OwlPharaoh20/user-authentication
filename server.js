704377681e01e40cb91f16a96590e94d7f2cee12
GG-Pharaoh <oxdiablo20@gmail.com>
2024-09-12 07:05:56 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json())
+
+const users = [ ]
+
+
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+
+app.post('/users', async  (req, res) => {
+    try {
+        const salt = 
+    }
+     const user = { name: req.body.name, password: req.body.password}
+     users.push(user)
+     res.status(201).send()
+     
+
+
+})
+
+
+app.listen(3000)
